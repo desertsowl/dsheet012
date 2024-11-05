5c67ae60e68dde67c1c78dda83944e8e69a50554
desertsowl <desertsowl@gmail.com>
2024-11-05 06:39:36 +0000

    Start

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const PORT = 5000;
 
-// Express settings
+// 使用するミドルウェア
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
@@ -14,20 +14,18 @@ app.use(session({
     saveUninitialized: true
 }));
 
+// express-ejs-layouts の設定
+app.use(expressLayouts);
+app.set('layout', 'layouts/main'); // デフォルトのレイアウトファイル
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Express EJS Layouts
-const expressLayouts = require('express-ejs-layouts');
-app.use(expressLayouts);
-app.set('layout', 'layouts/main');
-
-// Routes
+// ルート設定
 app.get('/', (req, res) => {
     res.render('login', { title: 'Login' });
 });
 
-// Start server
+// サーバーの起動
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
